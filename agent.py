4e92a285c1e0926b20ab2b26300ef9cb5d83c5dc
Matt <matthijs.wolters@yahoo.co.uk>
2022-10-25 11:02:28 +0200

    Reasoner started

--- agent.py
+++ agent.py
@@ -1,4 +1,14 @@
-import requests
-import owlready2
+#import requests
+from owlready2 import *
 
-print("Hello World")
+
+
+owlready2.JAVA_EXE = "C:\\Program Files (x86)\\Java\\jre1.8.0_351\\bin\\java.exe"
+owlready2.reasoning.JAVA_MEMORY = 1000
+
+#onto_path.append("Ontology_Intelligent_Agents_Group8.owl")
+
+onto = get_ontology("file://ontology_test.owl").load()
+
+sync_reasoner()
+
