6648ff221f8011e153134fc9b45f2826186f79e6
Matt <matthijs.wolters@yahoo.co.uk>
2022-11-01 12:12:37 +0100

    Current state

--- agent.py
+++ agent.py
@@ -1,5 +1,7 @@
-#import requests
+import requests
+import os
 from owlready2 import *
+
 
 
 
@@ -8,7 +10,41 @@ owlready2.reasoning.JAVA_MEMORY = 1000
 
 #onto_path.append("Ontology_Intelligent_Agents_Group8.owl")
 
-onto = get_ontology("file://ontology_test.owl").load()
+onto = get_ontology("file://Ontology_Intelligent_Agents_T-1.owl").load()
 
-sync_reasoner()
+with onto:
+    sync_reasoner()
 
+def main():
+    print("---Welcome to the IA Ontological Reasoner---\n")
+    #print("Select a query: ")
+    agent()
+
+def agent(query=""):
+    return
+
+def twitter(query=""):
+    # Optional params: start_time,end_time,since_id,until_id,max_results,next_token,
+    # expansions,tweet.fields,media.fields,poll.fields,place.fields,user.fields
+    query_params = {'query': '(from:twitterdev -is:retweet) OR #twitterdev','tweet.fields': 'author_id'}
+    search_url = "https://api.twitter.com/2/tweets/search/recent"    
+    requests.get()
+
+def bearer_oauth(r):
+    """
+    Method required by bearer token authentication.
+    """
+    bearer_token = os.environ.get("BEARER_TOKEN")
+    r.headers["Authorization"] = f"Bearer {bearer_token}"
+    r.headers["User-Agent"] = "v2RecentSearchPython"
+    return r
+
+def connect_to_endpoint(url, params):
+    response = requests.get(url, auth=bearer_oauth, params=params)
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.text)
+    return response.json()
+
+if __name__ == '__main__':
+    main()
